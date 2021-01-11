58a6728b4dd80ebd8de588a1a01f3ac6d427476f
Max Scher <maxobaxo@gmail.com>
2021-01-10 22:19:04 -0800

    modify heading

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -5,7 +5,8 @@ const Home = () => {
   
   return (
     <>
-      <p>Hello, World!</p>
+      <h1>Lazy Loading Image Demo</h1>
+      <p>This image is lazy, but these Jordans are legit.</p>
       <Image
         src="https://forge-homework.s3.amazonaws.com/fullsize.jpg"
         tempSrc="https://forge-homework.s3.amazonaws.com/thumb.jpg"
