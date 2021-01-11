d204e980404fac9acf682a6e85703b4d05eef63e
Max Scher <maxobaxo@gmail.com>
2021-01-10 16:02:46 -0800

    resolve basic aliases
    This commit adds aliases to webpack config, allowing imports from
    'Containers', 'Components', and 'Assets'

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import Home from './containers/Home';
+import Home from 'Containers/Home';
 
 ReactDOM.render(<Home />, document.getElementById('app'));
--- webpack.common.js
+++ webpack.common.js
@@ -12,6 +12,11 @@ module.exports = {
     filename: "[name].build.js",
   },
   resolve: {
+    alias: {
+      Assets: path.resolve(__dirname, 'src', 'assets'),
+      Components: path.resolve(__dirname, 'src', 'components'),
+      Containers: path.resolve(__dirname, 'src', 'containers'),
+    },
     extensions: [".js", ".jsx", ".json"],
   },
   module: {
