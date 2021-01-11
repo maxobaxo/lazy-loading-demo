7358d97b6c2c856a3de0579d66119b23fdc85d7d
Max Scher <maxobaxo@gmail.com>
2021-01-10 21:32:29 -0800

    add basic css & prop-types for Image component

--- src/components/Image.js
+++ src/components/Image.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
+import PropTypes from 'prop-types';
 
-const LazyLoadImage = ({ src, tempSrc, errSrc, alt }) => {
+const LazyLoadImage = ({ src, tempSrc, errSrc, alt, className }) => {
   const [imgSrc, setSrc] = useState(tempSrc);
   useEffect(() => {
     const img = new Image();
@@ -13,7 +14,21 @@ const LazyLoadImage = ({ src, tempSrc, errSrc, alt }) => {
     })
   }, [src, tempSrc]);
 
-  return <img src={imgSrc} alt={alt} />
+  return (
+    <img src={imgSrc} alt={alt} className={`lazy-load-img ${className}`} />
+  );
+}
+
+LazyLoadImage.propTypes = {
+  src: PropTypes.string.isRequired,
+  tempSrc: PropTypes.string.isRequired,
+  errSrc: PropTypes.string.isRequired,
+  alt: PropTypes.string.isRequired,
+  className: PropTypes.string
+}
+
+LazyLoadImage.defaultProps = {
+  className: '',
 }
 
 export default LazyLoadImage;
