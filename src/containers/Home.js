dd0b4f44bf9a2aec45dacf59b63e4318a53fd499
Max Scher <maxobaxo@gmail.com>
2021-01-10 21:06:49 -0800

    refactor use of Image component per lazy load

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import Image from 'Components/Image';
-import Thumb from 'Assets/thumb.jpg';
 
 const Home = () => {
+  
   return (
     <>
       <p>Hello, World!</p>
-      <Image src={Thumb} alt="shoes-thumbnail" />
+      <Image
+        src="https://forge-homework.s3.amazonaws.com/fullsize.jpg"
+        tempSrc="https://forge-homework.s3.amazonaws.com/thumb.jpg"
+        alt="Nike Air Jordans"
+      />
     </>
   );
 }
