f91235e50658d2094e58a94e3118547e373256e8
Max Scher <maxobaxo@gmail.com>
2021-01-10 21:06:29 -0800

    add lazy load functionality to Image component

--- src/components/Image.js
+++ src/components/Image.js
@@ -1,7 +1,19 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
-const Image = ({ src, alt }) => {
-  return <img src={src} alt={alt} />
+const LazyLoadImage = ({ src, tempSrc, errSrc, alt }) => {
+  const [imgSrc, setSrc] = useState(tempSrc);
+  useEffect(() => {
+    const img = new Image();
+    img.src = src;
+    img.addEventListener("load", function() {
+      setSrc(src);
+    });
+    img.addEventListener("error", function() {
+      setSrc(errSrd || tempSrc);
+    })
+  }, [src, tempSrc]);
+
+  return <img src={imgSrc} alt={alt} />
 }
 
-export default Image;
+export default LazyLoadImage;
