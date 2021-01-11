6f9eccb2e865060c21ba2ac9e86859b885a2f554
Max Scher <maxobaxo@gmail.com>
2021-01-10 21:33:47 -0800

    import global css at project entrypoint

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import Home from 'Containers/Home';
+import "./styles/index.css";
 
 ReactDOM.render(<Home />, document.getElementById('app'));
