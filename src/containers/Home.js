9233348f214ebcd4c72c215cfe91c6b32f6c5f84
Max Scher <maxobaxo@gmail.com>
2021-01-10 22:20:17 -0800

    rename the Image component as LazyImage

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,13 +1,13 @@
 import React from 'react';
-import Image from 'Components/Image';
+import LazyImage from 'Components/LazyImage';
 
 const Home = () => {
   
   return (
     <>
       <h1>Lazy Loading Image Demo</h1>
-      <p>This image is lazy, but these Jordans are legit.</p>
-      <Image
+      <p>This image is lazy, but the Jordans are legit.</p>
+      <LazyImage
         src="https://forge-homework.s3.amazonaws.com/fullsize.jpg"
         tempSrc="https://forge-homework.s3.amazonaws.com/thumb.jpg"
         alt="Nike Air Jordans"
